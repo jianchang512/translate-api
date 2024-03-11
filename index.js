f6d78052acd69302445049a245234de8da17471b
okmyworld <2124455076@qq.com>
2024-03-11 14:57:39 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+// 使用这里的 代码替换掉 worker 里左侧编辑区的代码即可
+
+import { Ai } from './vendor/@cloudflare/ai.js';
+
+// 这是访问密钥
+const SECRET_PASS="123456"
+
+export default {
+  async fetch(request, env) {
+    const urlStr = request.url
+    const urlObj = new URL(urlStr)
+    let text =  urlObj.searchParams.get('text')
+    let source_language = urlObj.searchParams.get('source_language')
+    let target_language = urlObj.searchParams.get('target_language')
+    let secret = urlObj.searchParams.get('secret')
+    if(secret!==SECRET_PASS){
+      return Response.json({code:1,msg:"无权访问",text:text,source_language:source_language,target_lanuage:target_language,secret:secret});
+    }
+
+    const ai = new Ai(env.AI);
+    const inputs = {
+      text: text,
+      source_lang: source_language,
+      target_lang: target_language
+    };
+    const response = await ai.run('@cf/meta/m2m100-1.2b', inputs);
+    if(response.translated_text.indexOf('ERROR')===0){
+      return Response.json({code:2,msg:"ok",text:response.translated_text});
+    }
+    return Response.json({code:0,msg:"ok",text:response.translated_text });
+  }
+};
